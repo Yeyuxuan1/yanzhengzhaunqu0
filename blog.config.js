e741090a588a276588f7239af69763e282654f9b
Yeyuxuan1 <130195420+Yeyuxuan1@users.noreply.github.com>
2023-08-28 03:26:06 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,13 +13,13 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '欢迎来到带路啦',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '帕兰朵', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个诚恳的老司机', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.papaxinxi.cc', // 网站地址
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '带路啦', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个认真负责的性息站', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.baidu.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '性息, 楼凤，修车大队，修车，老司机，小姐性息，全国信息，会所，品茶，茶馆，约炮', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
